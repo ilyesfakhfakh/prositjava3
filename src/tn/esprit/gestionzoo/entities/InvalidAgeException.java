94c46ceb7edb2dc6ffb29a2118540ee3b60285de
ilyes <mohamedilyes.fakhfakh@esprit.tn>
2023-10-30 15:35:53 +0100

    push prosit7

---
+++ src/tn/esprit/gestionzoo/entities/InvalidAgeException.java
@@ -0,0 +1,8 @@
+package tn.esprit.gestionzoo.entities;
+
+public class InvalidAgeException extends Exception {
+    public InvalidAgeException(){}
+    public InvalidAgeException(String msg){
+        super(msg);
+    }
+}
---
+++ src/tn/esprit/gestionzoo/entities/ZooFullException.java
@@ -0,0 +1,10 @@
+package tn.esprit.gestionzoo.entities;
+
+public class ZooFullException extends Exception{
+
+  public ZooFullException(){}
+    public ZooFullException(String msg){
+       super(msg);
+    }
+
+}
